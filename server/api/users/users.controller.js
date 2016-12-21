196743d164a087429f6598a029146025daa03376
alberto.blanco <alberto.blanco@bbva.com>
2016-12-21 19:05:05 +0100

    Added async library New service POST /api/users to generate a new VALID Ripple Wallet

--- server/api/users/users.controller.js
+++ server/api/users/users.controller.js
@@ -1,33 +1,38 @@
 'use strict';
 
+let async = require('async');
 const config = global.config;
 const rippleApi = config.rippleApi;
 
-var generateNewAddress = function() {
+let generateNewWallet = function(callback) {
   rippleApi.connect()
     .then(() => {
       /**
        * STEP 1: GENERATE RIPPLE ADDRESS
        */
-      rippleApi.generateAddress()
-        .then(info => {
-          rippleApi.disconnect().then(() => { console.log("Disconnected") }).catch(console.error);
-          return new Promise(info);
+      return rippleApi.generateAddress();
+    })
+    .then(info => {
+      rippleApi.disconnect()
+        .then(() => {
+          console.log("GENERATE ADDRESS. Ripple Disconnected");
+          callback(null, info);
         })
         .catch(error => {
-          //TODO: RETURN WHAT??
+          console.error(JSON.stringify(error));
+          callback(error, null);
         })
     })
     .catch(error => {
-      //TODO: RETURN WHAT??
+      console.error(JSON.stringify(error));
+      callback(error, null);
     })
 };
 
-var prepareAndSignPayment = function(sourceAddress, destAddress, amount, currency) {
+let createPayment = function(sourceAddress, sourceSecret, destAddress, amount, currency, callback) {
   rippleApi.connect()
     .then(() => {
-
-      var payment = {
+      let payment = {
         source: {
           address: sourceAddress,//config.rippleAccounts.activator.address,
           maxAmount: {
@@ -43,61 +48,115 @@ var prepareAndSignPayment = function(sourceAddress, destAddress, amount, currenc
           }
         }
       };
-      console.log(config.rippleAccounts.activator.address);
+      console.log("PREPARE PAYMENT:::::::::::::::::");
       console.log(JSON.stringify(payment));
-      return rippleApi.preparePayment(sourceAddress.address, payment);
+      console.log("::::::::::::::::::::::::::");
+      return rippleApi.preparePayment(sourceAddress, payment);
     }).then(preparedPayment => {
         /**
          * STEP 3: SIGN PREPARED PAYMENT
          */
-        return rippleApi.sign(preparedPayment.txJSON, sourceAddress.secret);
-    }).then((signedPayment) => {
-        rippleApi.disconnect().then(() => { console.log("Disconnected") }).catch(console.error);
-        return new Promise(signedPayment);
+        console.log("SIGN PAYMENT:::::::::::::::::");
+        console.log("::::::::::::::::::::::::::");
+        return rippleApi.sign(preparedPayment.txJSON, sourceSecret);
+    }).then(signedPayment => {
+      console.log("PAYMENT SIGNED:::::::::::::::::");
+      return rippleApi.submit(signedPayment.signedTransaction);
+    }).then((result) => {
+      console.log("PAYMENT SUBMITTED:::::::::::::::::");
+      rippleApi.disconnect()
+        .then(() => {
+          console.log("PAYMENT. Ripple Disconnected");
+          if(result.resultCode === "tesSUCCESS") {
+            callback(null, result);
+          }
+          else {
+            callback(result, null);
+          }
+        })
+        .catch(error => {
+          console.error(JSON.stringify(error));
+          callback(error, null);
+        })
     })
-    .catch(error => {
-      //TODO: RETURN WHAT??
+    .catch(rippleError => {
+      console.error(JSON.stringify(rippleError));
+      callback(rippleError, null);
     });
 };
 
 
-var prepareAndSignTrustline = function(sourceAddress, destAddress, currency) {
+let createTrustline = function(sourceAddress, sourceSecret, destAddress, currency, callback) {
   rippleApi.connect()
     .then(() => {
       const trustline = {
         "currency": currency,//"BEE",
-        "counterparty": destAddress.address,//config.rippleAccounts.coldBee.address,
+        "counterparty": destAddress,//config.rippleAccounts.coldBee.address,
         "limit": "100",
         "ripplingDisabled": false,
         "frozen": false
       };
-      return rippleApi.prepareTrustline(trustline, payment);
+      console.log("PREPARE TRUSTLINE:::::::::::::::::");
+      console.log("FROM: " + sourceAddress + " TO: " + destAddress + " FOR CURRENCY: " + currency);
+      console.log(JSON.stringify(trustline));
+      console.log("::::::::::::::::::::::::::");
+      return rippleApi.prepareTrustline(sourceAddress, trustline);
     }).then(preparedTrustline => {
-    /**
-     * STEP 3: SIGN PREPARED TRUSTLINE
-     */
-    return rippleApi.sign(preparedTrustline.txJSON, sourceAddress.secret);
-  }).then((signedTrustline) => {
-    rippleApi.disconnect().then(() => { console.log("Disconnected") }).catch(console.error);
-    return new Promise(signedTrustline);
-  })
+      /**
+       * STEP 3: SIGN PREPARED TRUSTLINE
+       */
+      console.log("SIGN TRUSTLINE:::::::::::::::::");
+      console.log("FROM: " + sourceAddress + " TO: " + destAddress + " FOR CURRENCY: " + currency);
+      console.log("::::::::::::::::::::::::::");
+      return rippleApi.sign(preparedTrustline.txJSON, sourceSecret);
+    }).then(signedTrustline => {
+        console.log("TRUSTLINE SIGNED:::::::::::::::::");
+        return rippleApi.submit(signedTrustline.signedTransaction);
+    }).then((result) => {
+      console.log("TRUSTLINE SUBMITTED:::::::::::::::::");
+      rippleApi.disconnect()
+        .then(() => {
+          console.log("TRUSTLINE. Ripple Disconnected");
+          if (result.resultCode === "tesSUCCESS") {
+            callback(null, result);
+          }
+          else {
+            callback(result, null);
+          }
+        })
+        .catch(error => {
+          console.error(JSON.stringify(error));
+          callback(error, null);
+        })
+    })
     .catch(error => {
-      //TODO: RETURN WHAT??
+      callback(error, null);
     });
 };
 
-var combineAndSubmitSignedTransactions = function(signedTransaction) {
+let combineAndSubmitSignedTransactions = function(signedTransaction, callback) {
   rippleApi.connect()
     .then(() => {
-    return rippleApi.combine(signedTransaction);
-  }).then(joinedTransaction => {
-    return rippleApi.submit(joinedTransaction.signedTransaction);
-  }).then((result) => {
-    rippleApi.disconnect().then(() => { console.log("Disconnected") }).catch(console.error);
-    return new Promise(result);
-  })
+      return rippleApi.combine(signedTransaction);
+    }).then(joinedTransaction => {
+      console.log("JOINING TRANSACTIONS:::::::::::::::::");
+      console.log("::::::::::::::::::::::::::");
+      return rippleApi.submit(joinedTransaction.signedTransaction);
+    }).then((result) => {
+      console.log("SUBMITTING TRANSACTIONS:::::::::::::::::");
+      console.log("::::::::::::::::::::::::::");
+      rippleApi.disconnect()
+        .then(() => {
+          console.log("SUBMIT TRANSACTION Disconnected");
+          callback(null, result);
+        })
+        .catch(error => {
+          console.error(JSON.stringify(error));
+          callback(error, null);
+        });
+    })
     .catch(error => {
-      //TODO: RETURN WHAT??
+      callback(error, null);
     });
 };
 
@@ -128,9 +187,9 @@ export function list(req, res) {
 }
 
 export function create(req, res) {
-  var email = req.body.email;
-  var currency = req.body.currency;
-
+  let email = req.body.email;
+  let currency = req.body.currency;
+  let coldWallet = (currency === "BEE") ? config.rippleAccounts.coldBee : config.rippleAccounts.coldGee;
   /**
    * STEPS:
    *  1.- Call generateNewAddress to get a new Address and Secret and keep both
@@ -146,77 +205,42 @@ export function create(req, res) {
    *      }
    *
    */
-
-  res.status(200).send("OK");
-  // rippleApi.connect()
-  //   .then(() => {
-  //     /**
-  //      * STEP 1: GENERATE RIPPLE ADDRESS
-  //      */
-  //     return rippleApi.generateAddress();
-  //
-  //   }).then(info => {
-  //   console.log(JSON.stringify(info));
-  //   console.log('getAccountInfo done');
-  //   var wallet = {"secret":"snwEQnV34E189nvZmw5eDKYCr5tH3","address":"rQaVEm4GfG39VHNMWZGKrNXMxicu7BXE9D"};
-  //
-  //     /**
-  //      *
-  //      * STEP 2: PREPARE PAYMENT
-  //      */
-  //
-  //   var payment = {
-  //     source: {
-  //       address: config.rippleAccounts.activator.address,
-  //       maxAmount: {
-  //        value: "40",
-  //        currency: "XRP"
-  //        }
-  //     },
-  //     destination: {
-  //       address: wallet.address,
-  //       amount: {
-  //         value: "40",
-  //         currency: "XRP"
-  //       }
-  //     }
-  //   };
-  //   console.log(config.rippleAccounts.activator.address);
-  //   console.log(JSON.stringify(payment));
-  //   return rippleApi.preparePayment(config.rippleAccounts.activator.address, payment);
-  // }).then((preparedPayment) => {
-  //   /**
-  //    * STEP 3: SIGN PREPARED PAYMENT
-  //    */
-  //   return rippleApi.sign(preparedPayment.txJSON, config.rippleAccounts.activator.secret);
-  // }).then((signedPayment) => {
-  //   /**
-  //    * STEP 4: PREPARE TRUSTLINE FROM NEW WALLET TO COLD WALLET
-  //    */
-  //   const trustline = {
-  //     "currency": "BEE",
-  //     "counterparty": config.rippleAccounts.coldBee.address,
-  //     "limit": "100",
-  //     "ripplingDisabled": false,
-  //     "frozen": false
-  //   };
-  //   return rippleApi.submit(signedPayment.signedTransaction)
-  // }).then((signedPayment) => {
-  //   /**
-  //    * OBJECT SIGNED. NEXT STEP, SUBMIT IT
-  //    */
-  //   return rippleApi.submit(signedPayment.signedTransaction)
-  // }).then((transactionSubmitted) => {
-  //   /**
-  //    * OBJECT SIGNED. NEXT STEP, SUBMIT IT
-  //    */
-  //   res.status(200).send(transactionSubmitted);
-  // }).then(() => {
-  //   return rippleApi.disconnect();
-  // }).then(() => {
-  //   console.log("Disconnected")
-  // }).catch((error) => {
-  //   console.log(JSON.stringify(error));
-  //   res.status(500).send(error);
-  // });
+  generateNewWallet(function (errorGenWallet, newWallet){
+    if(errorGenWallet) {
+      res.status(500).json(errorGenWallet);
+    }
+    else {
+      console.log("NEW WALLET:::::::::::::::::");
+      console.log(JSON.stringify(newWallet));
+      console.log("::::::::::::::::::::::::::");
+      async.series([
+        function (callback) {
+          createPayment(config.rippleAccounts.activator.address, config.rippleAccounts.activator.secret, newWallet.address, "40", 'XRP', callback);
+        },
+        function (callback) {
+          createTrustline(newWallet.address, newWallet.secret, coldWallet.address, currency, callback);
+        },
+        function (callback) {
+          createTrustline(coldWallet.address, coldWallet.secret, newWallet.address, currency, callback);
+        }
+      ],
+      function (errorSerie, resultSerie) {
+        if(errorSerie) {
+          res.status(500).json(errorSerie);
+        }
+        else {
+          /**
+           * TODO: INSERT IN DB
+           */
+          let response = {
+            user: {
+              email: req.body.email,
+              wallet: newWallet.address
+            }
+          };
+          res.status(200).send(response);
+        }
+      });
+    }
+  });
 }
